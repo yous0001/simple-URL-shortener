f7fb582f857ab0d64663c16c7cc35a9c523c7f9b
Yousef emad mikhail <mad33833@gmail.com>
2025-02-17 21:25:07 +0200

    change apis responcies

--- controllers/shorter.contorller.js
+++ controllers/shorter.contorller.js
@@ -5,7 +5,7 @@ import urlModel from "../DB/models/url.model.js"
 export const shorterURL=async(req,res,next) => {
     const {longUrl}=req.body
 
-    if(!longUrl)return res.status(404).json({message:"please provide url"})
+    if(!longUrl)return res.status(404).json({message:"please provide longUrl"})
 
     const existingUrl=await urlModel.findOne({longUrl})
     if(existingUrl) return res.status(409).json({message:"url already exists"})
@@ -14,7 +14,7 @@ export const shorterURL=async(req,res,next) => {
     const urlCode=nanoid(6)
     const shortUrl= `${process.env.BASE_URL}/${urlCode}`
     const newUrl=await urlModel.create({longUrl,shortUrl,urlCode})
-    
+    if(!newUrl)return res.status(404).json({message:"url creation failed"})
     res.status(201).json({shortUrl:newUrl.shortUrl})
     
 }
