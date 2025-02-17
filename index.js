34705a555d615672499d7e4c2613ac90b094f0c9
Yousef emad mikhail <mad33833@gmail.com>
2025-02-17 21:11:45 +0200

    add global responce middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import db_connection from "./DB/db_connection.js"
 import shorterRouter from "./Routes/shorter.router.js"
 import { config } from "dotenv"
+import { globalResponse } from "./middlewares/globalResponce.middleware.js"
 
 config()
 const port = process.env.PORT || 3000
@@ -9,6 +10,11 @@ const app=express()
 app.use(express.json())
 app.use("/url",shorterRouter)
 
+app.use('*',(req,res,next)=>{
+    res.status(404).json({message:"end point not found"})
+})
+
+app.use(globalResponse)
 db_connection()
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`)
